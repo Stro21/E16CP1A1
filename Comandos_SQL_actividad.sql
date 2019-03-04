bf5dea327448f0a50fcdbb11cbc410a4eeef3389
Eduardo Straub <eduardo.straub1@gmail.com>
2019-03-04 19:51:24 -0300

    Se agregan las instrucciones de SQL para la actividad.

---
+++ Comandos_SQL_actividad.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE call_list;
+CREATE TABLE users(id integer, first_name varchar(10), email varchar(64));
+INSERT INTO users (id, first_name, email) VALUES (1, 'Carlos', 'carlos@correo.com');
+INSERT INTO users (id, first_name, email) VALUES (1, 'Laura', 'laura@correo.com');
