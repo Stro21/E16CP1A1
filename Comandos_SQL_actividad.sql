cb2105e042aff5a70bccc159e56d762ce21cde21
Eduardo Straub <eduardo.straub1@gmail.com>
2019-03-04 20:17:16 -0300

    Se modificarón las id para sean generadas automaticamente.

--- Comandos_SQL_actividad.sql
+++ Comandos_SQL_actividad.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE call_list;
-CREATE TABLE users(id integer, first_name varchar(10), email varchar(64));
-INSERT INTO users (id, first_name, email) VALUES (1, 'Carlos', 'carlos@correo.com');
-INSERT INTO users (id, first_name, email) VALUES (2, 'Laura', 'laura@correo.com');
+CREATE TABLE users (id serial primary key, first_name varchar(10), email varchar(64));
+INSERT INTO users (first_name, email) VALUES ('Carlos', 'carlos@correo.com');
+INSERT INTO users (first_name, email) VALUES ('Laura', 'laura@correo.com');
+CREATE TABLE calls (id serial primary key, phone varchar(12), date date, user_id integer REFERENCES users(id));
 
