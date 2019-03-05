68ec3f4738b8b005f720eb535ed62828e120ac71
Eduardo Straub <eduardo.straub1@gmail.com>
2019-03-04 21:36:13 -0300

    Se agrego un nuevo usuario.

--- Comandos_SQL_actividad.sql
+++ Comandos_SQL_actividad.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE call_list;
 CREATE TABLE users (id serial primary key, first_name varchar(10), email varchar(64));
-INSERT INTO users VALUES ('Carlos', 'carlos@correo.com');
-INSERT INTO users VALUES ('Laura', 'laura@correo.com');
+INSERT INTO users (first_name, email) VALUES ('Carlos', 'carlos@correo.com');
+INSERT INTO users (first_name, email) VALUES ('Laura', 'laura@correo.com');
 CREATE TABLE calls (id serial primary key, phone varchar(20), date date, user_id integer REFERENCES users(id));
 ALTER TABLE users ADD COLUMN last_name varchar(10);
 UPDATE users SET last_name = 'Espinoza' WHERE first_name = 'Carlos';
@@ -16,3 +16,4 @@ INSERT INTO calls (phone, date, user_id) VALUES ('988776655', '2019-03-04', 1);
 INSERT INTO calls (phone, date, user_id) VALUES ('988776655', '2019-03-04', 1);
 INSERT INTO calls (phone, date, user_id) VALUES ('988776655', '2019-03-04', 1);
 INSERT INTO calls (phone, date, user_id) VALUES ('988776655', '2019-03-04', 1);
+INSERT INTO users (first_name, email, last_name) VALUES ('Eduardo', 'eduardo.straub1@gmail.com', 'Straub');
