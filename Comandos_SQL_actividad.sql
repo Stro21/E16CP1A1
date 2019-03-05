cd74120e1857ba53ba10aab9dc25570c6d573d35
Eduardo Straub <eduardo.straub1@gmail.com>
2019-03-04 21:29:10 -0300

    Se agrego 6 llamadas para Laura.

--- Comandos_SQL_actividad.sql
+++ Comandos_SQL_actividad.sql
@@ -1,9 +1,14 @@
 CREATE DATABASE call_list;
 CREATE TABLE users (id serial primary key, first_name varchar(10), email varchar(64));
-INSERT INTO users (first_name, email) VALUES ('Carlos', 'carlos@correo.com');
-INSERT INTO users (first_name, email) VALUES ('Laura', 'laura@correo.com');
-CREATE TABLE calls (id serial primary key, phone varchar(12), date date, user_id integer REFERENCES users(id));
+INSERT INTO users VALUES ('Carlos', 'carlos@correo.com');
+INSERT INTO users VALUES ('Laura', 'laura@correo.com');
+CREATE TABLE calls (id serial primary key, phone varchar(20), date date, user_id integer REFERENCES users(id));
 ALTER TABLE users ADD COLUMN last_name varchar(10);
 UPDATE users SET last_name = 'Espinoza' WHERE first_name = 'Carlos';
 UPDATE users SET last_name = 'Gonzales' WHERE first_name = 'Laura';
-
+INSERT INTO calls (phone, date, user_id) VALUES ('987654321', '2019-03-04', 2);
+INSERT INTO calls (phone, date, user_id) VALUES ('987654321', '2019-03-04', 2);
+INSERT INTO calls (phone, date, user_id) VALUES ('987654321', '2019-03-04', 2);
+INSERT INTO calls (phone, date, user_id) VALUES ('987654321', '2019-03-04', 2);
+INSERT INTO calls (phone, date, user_id) VALUES ('987654321', '2019-03-04', 2);
+INSERT INTO calls (phone, date, user_id) VALUES ('987654321', '2019-03-04', 2);
